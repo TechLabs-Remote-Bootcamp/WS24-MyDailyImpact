dd924fef4c6aadf28f6977a2e0062811ec43dabb
Vivian Sandler <vivianmargothsandler@gmail.com>
2025-01-14 23:34:18 +0000

    feature: added left/right component page format to login page

---
+++ mdi-react/src/components/RC_login.jsx
@@ -0,0 +1,11 @@
+export default function RC_login() {
+    return (
+    <div className="login-container">
+      <h2>Welcome to the login.</h2>
+      <form className="form log-in" action="">
+        <input className="email" type="email" placeholder="Email" />
+        <input type="password"/> 
+      </form>
+    </div>
+    );
+}
--- mdi-react/src/components/pages/Login.jsx
+++ mdi-react/src/components/pages/Login.jsx
@@ -1,16 +1,21 @@
 import "./Login.scss";
 import Content_LR from "../Content_LR";
+import LC_bigImage from "../LC_bigImage";
+import RC_login from "../RC_login.jsx";
 import Button from "../Button.jsx";
 
 export default function Login() {
   return (
-    <>
-      <h2>Welcome to the login.</h2>
-      <form className="form log-in" action="">
-        <input className="email" type="email" placeholder="Email" />
-        <input type="password"/> 
-      </form>
+    <Content_LR
+      LeftComponent={
+        <LC_bigImage rotationOn={false} overflowText="My Daily Impact" />
+      }
+      RightComponent= {
+      <>
+      <RC_login />
       <Button>Log in</Button>
-    </>
+      </>
+      }
+      />
   );
 }
