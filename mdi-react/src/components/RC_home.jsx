c480b6eec92c6c9bb0b9e26ba1a3b40c4c7f9f6f
Vivian Sandler <vivianmargothsandler@gmail.com>
2025-01-15 02:04:58 +0000

    design: adding styling to colored containers

--- mdi-react/src/components/RC_home.jsx
+++ mdi-react/src/components/RC_home.jsx
@@ -20,19 +20,3 @@ export default function RC_Home() {
     </div>
   );
 }
-
-//   return (
-//     <div className="content-container">
-//       <div className="green-box">Get ready for your impact journey</div>
-//       <div className="blue-box">Every plant-based meal counts</div>
-//       <div className="text-box">
-//         <p>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-//           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-//           aliquip ex ea commodo consequat.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
