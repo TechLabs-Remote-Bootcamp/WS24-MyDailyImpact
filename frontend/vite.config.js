12f7f65a7bcaf69f47731c0454eb8d273d97ec38
tomi <tomislav.varga@icloud.com>
2025-01-15 12:38:07 +0100

    Add: dotenv module and configure to load .env file in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 export default ({ mode }) => {
-  // Load environment variables for the current mode
-  const env = loadEnv(mode, process.cwd());
+  const port = process.env.APP_PORT;
 
   return defineConfig({
     root: 'frontend',
@@ -14,12 +19,12 @@ export default ({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: `http://127.0.0.1:${env.VITE_API_PORT || '5000'}`, // Use the loaded env variable
+          target: `http://127.0.0.1:${port || '5000'}`,
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
-    },
+        }
+      }
+    }
   });
 };
