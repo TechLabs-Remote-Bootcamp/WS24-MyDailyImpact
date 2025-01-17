fb5ceba6d3db2018da79c2a1c3fd32d10fcf06c5
Vivian Sandler <vivianmargothsandler@gmail.com>
2025-01-17 19:40:28 +0000

    docs: updated text in home page

--- mdi-react/src/components/RC_home.jsx
+++ mdi-react/src/components/RC_home.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import ColoredContainers from "./Colored-Containers";
-import "../styles/rc_general.scss";
 import "./Colored-Containers.scss";
+import Button from "./Button";
+import { Link } from "react-router-dom";
 
 export default function RC_Home() {
   return (
     <div>
       <ColoredContainers
-      h2Text="Get ready for your impact journey"
-      h3Text="Every plant-based meal counts"
+        h2Text="Get ready for your impact journey"
+        h3Text="Every plant-based meal counts"
       >
-      <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-      </p>
+        <div className="home-text">
+          <p>Did you know that</p>
+          <ul className="home-list">
+            <li>
+              by switching out meat-based meals with plant-based ones, you can
+              have a positive impact on the environment and animals’ lives?
+            </li>
+            <li>
+              you don’t have to be fully plant-based or vegan to make a positive
+              difference?
+            </li>
+            <li>every meal you eat counts?</li>
+          </ul>
+          <p className="bold">
+            Join us at MyDailyImpact, where you can track how many animals’
+            lives and how much forest land, CO2 emission, and water you save by
+            swapping out meat- for plant-based meals!
+          </p>
+          <Button>
+            <Link to="/sign-up">Join us!</Link>
+          </Button>
+        </div>
       </ColoredContainers>
     </div>
   );
