40557653b49985d7d692147fc3c267b38304aa05
tomi <tomislav.varga@icloud.com>
2025-01-18 15:37:59 +0100

    update: .env variable and proxy setting in vite.config.js

--- mdi-react/vite.config.js
+++ mdi-react/vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+const port = process.env.APP_PORT;
+console.log('Server running on port'+ port);
+console.log('Path:', __dirname);
+
 export default defineConfig({
-  plugins: [react()],
-  base: './'
-})
+    plugins: [react()],
+    base: './',
+    server: {
+      proxy: {
+        '/api': {
+          target: `http://127.0.0.1:${port || '5000'}`,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    }
+  });
