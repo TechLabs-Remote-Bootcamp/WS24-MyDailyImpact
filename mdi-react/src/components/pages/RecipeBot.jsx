103853fcb0ffab0d9b62ba46bcf31603c34a0811
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-01-20 13:52:44 +0100

    fix: function name

--- mdi-react/src/components/pages/RecipeBot.jsx
+++ mdi-react/src/components/pages/RecipeBot.jsx
@@ -1,6 +1,6 @@
 import "./RecipeBot.scss";
 import Content_LR from "../Content_LR";
 
-export default function About() {
-  return <h1>Ask me for tasty plant-based recipes!</h1>;
+export default function RecipeBot() {
+  return <h4>Ask me for tasty plant-based recipes!</h4>;
 }
