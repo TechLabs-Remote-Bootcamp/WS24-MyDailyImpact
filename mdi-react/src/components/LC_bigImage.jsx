2c32dbcede980a188a678d1c8690f4d9f8c9566c
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-01-21 13:43:35 +0100

    refactor: delete redundant html

--- mdi-react/src/components/LC_bigImage.jsx
+++ mdi-react/src/components/LC_bigImage.jsx
@@ -1,23 +1,15 @@
 import treeworld_big from "../images/treeWorld1250.png";
-import treeworld_small from "../images/treeWorld.png";
 import "./LC_bigImage.scss";
 
 function LC_bigImage({ overflowText, textClass }) {
   return (
     <>
       <img
-        id="treeWorld-big"
+        id="treeWorld"
         className="image-box"
         src={treeworld_big}
         alt="A painted earth with many different trees along its circumfence line"
       />
-      {/* <img
-        id="treeWorld-small"
-        className="image-box"
-        src={treeworld_small}
-        alt="A painted earth with many different trees along its circumfence line"
-      /> */}
-      {/* <p className={textClass}>{overflowText}</p> */}
     </>
   );
 }
