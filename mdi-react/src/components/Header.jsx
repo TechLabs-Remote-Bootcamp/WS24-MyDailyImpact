1419858bb409e5ac386265498b337b8f134fedc2
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-01-21 13:45:31 +0100

    refactor: first steps for implementing mobile header

--- mdi-react/src/components/Header.jsx
+++ mdi-react/src/components/Header.jsx
@@ -4,49 +4,54 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="outer-header-grid">
-      <div className="logo">
-        <img src={logo} />
-      </div>
-      <div className="heaven-stripe"></div>
-      <nav className="leaves">
-        <li className="single">
-          <Link to="/" className="leaf-surface">
-            <span>Home</span>
-          </Link>
-          <div className="leaf-stem"></div>
-        </li>
-        <li className="single">
-          <Link to="/about" className="leaf-surface">
-            <span>About us</span>
-          </Link>
-          <div className="leaf-stem"></div>
-        </li>
-        <li className="single">
-          <Link to="/recipeBot" className="leaf-surface">
-            <span>Recipe Bot</span>
-          </Link>
-          <div className="leaf-stem"></div>
-        </li>
-        <li className="single">
-          <Link to="/login" className="leaf-surface">
-            <span>Login</span>
-          </Link>
-          <div className="leaf-stem"></div>
-        </li>
-        <li className="single">
-          <Link to="/sign-up" className="leaf-surface">
-            <span>Sign up</span>
-          </Link>
-          <div className="leaf-stem"></div>
-        </li>
-      </nav>
-      <div className="plant-stem"></div>
-      <div className="blossom">
-        <div className="b-1"></div>
-        <div className="b-2"></div>
-        <div className="b-3"></div>
-      </div>
-    </header>
+    <>
+      <header id="header-desktop" className="flower-header-grid">
+        <div className="logo">
+          <img src={logo} />
+        </div>
+        <div className="heaven-stripe"></div>
+        <nav className="leaves">
+          <li className="single">
+            <Link to="/" className="leaf-surface">
+              <span>Home</span>
+            </Link>
+            <div className="leaf-stem"></div>
+          </li>
+          <li className="single">
+            <Link to="/about" className="leaf-surface">
+              <span>About us</span>
+            </Link>
+            <div className="leaf-stem"></div>
+          </li>
+          <li className="single">
+            <Link to="/recipeBot" className="leaf-surface">
+              <span>Recipe Bot</span>
+            </Link>
+            <div className="leaf-stem"></div>
+          </li>
+          <li className="single">
+            <Link to="/login" className="leaf-surface">
+              <span>Login</span>
+            </Link>
+            <div className="leaf-stem"></div>
+          </li>
+          <li className="single">
+            <Link to="/sign-up" className="leaf-surface">
+              <span>Sign up</span>
+            </Link>
+            <div className="leaf-stem"></div>
+          </li>
+        </nav>
+        <div className="plant-stem"></div>
+        <div className="blossom">
+          <div className="b-1"></div>
+          <div className="b-2"></div>
+          <div className="b-3"></div>
+        </div>
+      </header>
+      <header id="header-mobile">
+        <h2>Hello</h2>
+      </header>
+    </>
   );
 }
