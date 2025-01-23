497220788fd9df1865084dd160fcdb0df862573f
Vivian Sandler <vivianmargothsandler@gmail.com>
2025-01-23 23:33:12 +0000

    fix: fixed minor bugs that prevented browser from rendering

--- mdi-react/src/components/RC_login.jsx
+++ mdi-react/src/components/RC_login.jsx
@@ -4,7 +4,6 @@ import ColoredContainers from './Colored-Containers';
 import { useAuth } from '../hooks/useAuth';
 import { useNavigate } from "react-router-dom";
 import "./Colored-Containers.module.scss";
-import "./RC_login.module.scss";
 
 export default function RC_login() {
   const [email, setEmail] = useState("");
