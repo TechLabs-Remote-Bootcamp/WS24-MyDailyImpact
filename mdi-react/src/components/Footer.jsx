fc72bf8d177ff5835fa74a606e57dc24179b1899
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-01-23 13:08:35 +0100

    refactor: adjust to scss modules with small changes needed in the jsx file

--- mdi-react/src/components/Footer.jsx
+++ mdi-react/src/components/Footer.jsx
@@ -1,27 +1,25 @@
-import "./Footer.module.scss";
+import styles from "./Footer.module.scss";
 import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p className="copyright">
-          &copy; {new Date().getFullYear()} MyDailyImpact
-        </p>
-        <nav>
-          <ul className="footer-nav">
-            <li className="footer-nav-item">
-              <NavLink to="/contact-us">Contact</NavLink>
-            </li>
-            <li className="footer-nav-item">
-              <NavLink to="/imprint">Imprint</NavLink>
-            </li>
-            <li className="footer-nav-item">
-              <NavLink to="/privacy">Privacy</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>
+        &copy; {new Date().getFullYear()} MyDailyImpact
       </div>
+      <nav>
+        <ul className={styles["footer-links"]}>
+          <li className={styles["footer-nav-item"]}>
+            <NavLink to="/contact-us">Contact</NavLink>
+          </li>
+          <li className={styles["footer-nav-item"]}>
+            <NavLink to="/imprint">Imprint</NavLink>
+          </li>
+          <li className={styles["footer-nav-item"]}>
+            <NavLink to="/privacy">Privacy</NavLink>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 }
