6ff505f31fd5fc304f38a1304ce5d95c9100e5fa
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-01-23 13:09:58 +0100

    fix: delete empty file and its import

--- mdi-react/src/components/pages/Imprint.jsx
+++ mdi-react/src/components/pages/Imprint.jsx
@@ -1,7 +1,6 @@
 import Content_LR from "../Content_LR";
 import LC_bigImage from "../LC_bigImage";
 import RC_imprint from "../RC_imprint.jsx";
-import "./Imprint.scss";
 
 export default function Imprint() {
   return (
