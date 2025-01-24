300f5c1b8040cce55b0a8068a88a1b709539f266
Vivian Sandler <vivianmargothsandler@gmail.com>
2025-01-24 13:11:19 +0000

    fix: removed import of Button component

--- mdi-react/src/components/pages/SignUp.jsx
+++ mdi-react/src/components/pages/SignUp.jsx
@@ -2,7 +2,6 @@ import "./SignUp.module.scss";
 import Content_LR from "../Content_LR.jsx";
 import LC_bigImage from "../LC_bigImage.jsx";
 import RC_signup from "../RC_signup.jsx";
-import Button from "../Button.jsx";
 
 export default function Signup() {
   return (
