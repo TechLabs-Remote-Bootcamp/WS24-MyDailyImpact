fdc76a6bce582c4075ce1ff45ad1914ba054acb4
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-01-29 13:02:50 +0100

    refactor: classname as prop and primary button as default

--- mdi-react/src/components/Button.jsx
+++ mdi-react/src/components/Button.jsx
@@ -1,30 +1,13 @@
-// import PropTypes from 'prop-types';
-
-// function Button ({ Component, children }) {
-//   return (
-//     <>
-//     <Component/>
-//     <p>{children}</p>
-//     </>
-//   );
-// }
-
-// Button.propTypes = {
-//     children: PropTypes.node,
-//     Component: PropTypes.Component
-// }
-
-// export default Button;
-
 import styles from "./Button.module.scss";
 
-const Button = ({ children, onClick, disabled = false }) => {
+const Button = ({
+  children,
+  onClick,
+  disabled = false,
+  className = "primary-button",
+}) => {
   return (
-    <button
-      className={styles["primary-button"]}
-      onClick={onClick}
-      disabled={disabled}
-    >
+    <button className={styles[className]} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
