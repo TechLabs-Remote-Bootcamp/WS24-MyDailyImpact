2a5489caeaa7bca4387aba472ee4fede059f4013
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-02-11 09:14:42 +0100

    insert useLocation for getting userId from dashboard

--- mdi-react/src/components/pages/MealLogging/RC_mealLog.jsx
+++ mdi-react/src/components/pages/MealLogging/RC_mealLog.jsx
@@ -1,104 +1,172 @@
+import { useAuth } from "../../../hooks/useAuth";
+import { useNavigate, useLocation } from "react-router-dom";
+import { jwt } from "../../../utils/jwt";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import ColoredContainers from "../../core/ColoredContainers/Colored-Containers";
 import Button from "../../core/Button/Button";
-
+import { ApiError, api } from "../../../utils/api";
 import form from "../../../styles/forms.module.scss";
-import { useState, useEffect } from "react";
-import { useForm, useFieldArray, Controller } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 
 export default function RC_MealLog() {
+  const location = useLocation();
+  const user = location.state.userId.userId || "";
+  const navigate = useNavigate();
+  //const { isAuthenticated, user, logout, loading, initializeAuth } = useAuth();
   const {
     register,
-    formState,
     formState: { errors, isSubmitSuccessful },
     handleSubmit,
     reset,
   } = useForm({
     mode: "onSubmit",
     defaultValues: {
-      mealName: "",
-      mealType: "Breakfast",
-      comment: "",
+      user: user,
+      meal: "",
+      //mealType: "Breakfast",
+      date: new Date(),
+      notes: "",
     },
   });
 
-  const mealTypes = ["Breakfast", "Lunch", "Dinner"];
-
-  const onSubmitting = (data) => {
-    console.log(data);
-  };
-
   // useEffect(() => {
-  //   if (formState.isSubmitSuccessful) {
-  //     reset();
+  //   console.log(
+  //     "Dashboard useEffect - isAuthenticated:",
+  //     isAuthenticated,
+  //     "user:",
+  //     user,
+  //     "loading:",
+  //     loading
+  //   );
+  //   if (!isAuthenticated && !loading) {
+  //     initializeAuth();
   //   }
-  // }, [formState, submittedData, reset]);
+  // }, [isAuthenticated, loading, initializeAuth, user]);
 
-  return (
-    <>
-      <ColoredContainers
-        h2Text="What are you eating today?"
-        h3Text="Choose the right input and save."
-      >
-        <form
-          className={form["formpage-grid"]}
-          onSubmit={handleSubmit(onSubmitting)}
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (!isAuthenticated) {
+  //   navigate("/login");
+  // }
+
+  const mealTypes = ["Breakfast", "Lunch", "Dinner"];
+  //const now = new Date();
+
+  const onSubmit = (data) => {
+    const dataToSend = {
+      ...data,
+      user: user,
+    };
+
+    console.log(data);
+    const token = jwt.getToken();
+    console.log(token);
+    //try {
+    //setValue(dataToSend.date, now);
+
+    //   fetch("http://localhost:5001/api/meal-logs", {
+    //     method: "POST",
+    //     credentials: "include",
+    //     headers: {
+    //       Authorization: token ? `Bearer ${token}` : "",
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(dataToSend),
+    //   }).then(() => {
+    //     console.log("Meal data successful transferred");
+    //     navigate("/dashboard");
+    //   });
+    // };
+
+    //   dataToSend
+    // );
+    // if (response) {
+    //   console.log("Meal data successful transferred");
+    // }
+    //   } catch (error) {
+    //     console.error("Error:", error);
+    //     if (error instanceof ApiError) {
+    //       console.error("API Error Status:", error.status);
+    //       console.error("API Error Message:", error.message);
+    //     }
+    //   }
+    // };
+
+    // useEffect(() => {
+    //   if (formState.isSubmitSuccessful) {
+    //     reset();
+    //   }
+    // }, [formState, submittedData, reset]);
+
+    return (
+      <>
+        <ColoredContainers
+          // h2Text="What are you eating today?"
+          // h3Text="Choose the right input and save."
+          h2Text={user}
+          h3Text="Hallo"
         >
-          <section className={form.formSection}>
-            <div className={form.inputSection}>
-              <label className={form.label}>Meal name:</label>
-              <input
-                className={form.input}
-                name="mealName"
-                {...register("mealName", { required: true, maxLength: 50 })}
-              ></input>
-            </div>
-            <div className={form.inputSection}>
-              <label className={form.label}>Meal of the day:</label>
-              <div className={form.radioGroup}>
-                {mealTypes.map((meal) => (
-                  <label key={meal} className={form.radioLabel}>
-                    <input
-                      type="radio"
-                      name="mealType"
-                      value={meal}
-                      {...register("mealType", { required: true })}
-                      style={{
-                        height: "1.2rem",
-                        width: "1.2rem",
-                      }}
-                    />
-                    {meal}
-                  </label>
-                ))}
+          <form
+            className={form["formpage-grid"]}
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <section className={form.formSection}>
+              <div className={form.inputSection}>
+                <label className={form.label}>Meal name:</label>
+                <input
+                  className={form.input}
+                  name="meal"
+                  {...register("meal", { required: true, maxLength: 50 })}
+                ></input>
               </div>
-            </div>
-            <div className={form.inputSection}>
-              <label className={form.label}>Comment:</label>
-              <textarea
-                className={form.input}
-                style={{ height: "unset" }}
-                maxLength={500}
-                rows={5}
-                name="comment"
-                {...register("comment", { required: false, maxLength: 150 })}
-              ></textarea>
-            </div>
-          </section>
-          <section className={form.buttonSection}>
-            <Button
-              type="submit"
-              onClick={() => {
-                reset((formValues) => ({
-                  ...formValues,
-                }));
-              }}
-            >
-              Save
-            </Button>
-          </section>
-        </form>
-      </ColoredContainers>
-    </>
-  );
+              <div className={form.inputSection}>
+                <label className={form.label}>Meal of the day:</label>
+                <div className={form.radioGroup}>
+                  {mealTypes.map((meal) => (
+                    <label key={meal} className={form.radioLabel}>
+                      <input
+                        type="radio"
+                        name="mealType"
+                        value={meal}
+                        // {...register("mealType", { required: true })}
+                        // style={{
+                        //   height: "1.2rem",
+                        //   width: "1.2rem",
+                        // }}
+                      />
+                      {meal}
+                    </label>
+                  ))}
+                </div>
+              </div>
+              <div className={form.inputSection}>
+                <label className={form.label}>Comment:</label>
+                <textarea
+                  className={form.input}
+                  style={{ height: "unset" }}
+                  maxLength={500}
+                  rows={5}
+                  name="notes"
+                  {...register("notes", { required: false, maxLength: 150 })}
+                ></textarea>
+              </div>
+            </section>
+            <section className={form.buttonSection}>
+              <Button
+                type="submit"
+                onClick={() => {
+                  reset((formValues) => ({
+                    ...formValues,
+                  }));
+                }}
+              >
+                Save
+              </Button>
+            </section>
+          </form>
+        </ColoredContainers>
+      </>
+    );
+  };
 }
