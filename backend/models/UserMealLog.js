b19b0323ed80348426abd84e22e8efde24361307
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-02-12 16:01:34 +0100

    changed meal-log schema

--- backend/models/UserMealLog.js
+++ backend/models/UserMealLog.js
@@ -1,31 +1,42 @@
 import mongoose from 'mongoose';
 
 const userMealLogSchema = new mongoose.Schema({
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User', 
-      required: true
-    },
-    meal: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Meal',
-      required: true
-    },
-    date: {
-      type: Date,
-      required: true,
-      default: Date.now
-    },
-    notes: {
-      type: String,
-      trim: true,
-      default: ''
-    }
-  }, {
-    timestamps: true,
-    collection: 'user_meal_logs'
-  });
-  
-  const UserMealLog = mongoose.model('UserMealLog', userMealLogSchema);
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  mealId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Meal',
+    required: true
+  },
+  mealName: {
+    type: String,
+    trim: true,
+    required: true,
+    default: ''
+  },
+  category: {
+    type: String,
+    enum: ['Breakfast', 'Lunch', 'Dinner'],
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  notes: {
+    type: String,
+    trim: true,
+    default: ''
+  }
+}, {
+  timestamps: true,
+  collection: 'user_meal_logs'
+});
 
-  export default UserMealLog;
+const UserMealLog = mongoose.model('UserMealLog', userMealLogSchema);
+
+export default UserMealLog;
