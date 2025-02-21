67cc9e56564ed4a3be3b11a0fe6b10f85d9f0787
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-02-21 12:41:44 +0100

    delete ContentLR - just the table

--- mdi-react/src/components/pages/MealHistory/MealHistory.jsx
+++ mdi-react/src/components/pages/MealHistory/MealHistory.jsx
@@ -2,21 +2,17 @@ import Content_LR from "../../core/ContentLR/Content_LR.jsx";
 import ColoredContainers from "../../core/ColoredContainers/Colored-Containers.jsx";
 import LC_bigImage from "../../core/LC/LC_bigImage.jsx";
 import HistoryTable from "./HistoryTable.jsx";
+import styles from "./HistoryTable.module.scss";
 
 export default function MealHistory() {
   return (
-    <Content_LR
-      LeftComponent={
-        <LC_bigImage rotationOn={false} overflowText="My Daily Impact" />
-      }
-      RightComponent={
-        <ColoredContainers
-          h2Text="Check your meal history"
-          h3Text="Here you can see all the meals you have logged."
-        >
-          <HistoryTable />
-        </ColoredContainers>
-      }
-    />
+    <div className={styles.historyContainer}>
+      <ColoredContainers
+        h2Text="Check your meal history"
+        h3Text="Here you can see all the meals you have logged."
+      >
+        <HistoryTable />
+      </ColoredContainers>
+    </div>
   );
 }
