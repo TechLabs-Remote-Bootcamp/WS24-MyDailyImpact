7b36fd8eddd948649475f75f1215e8685e66be77
tomi <tomislav.varga@icloud.com>
2025-02-23 10:26:04 +0100

    add: put and delete operations in meal log

--- backend/routes/userMealLog.js
+++ backend/routes/userMealLog.js
@@ -1,12 +1,16 @@
 import express from "express";
 import {
   createMealLog,
-  getUserMealLogs
+  getUserMealLogs,
+  updateMealLog,
+  deleteMealLog,
 } from "../controllers/mealLog.js";
 
 const userMealLogRouter = express.Router();
 
 userMealLogRouter.post("/", createMealLog);
 userMealLogRouter.get("/:userId", getUserMealLogs);
+userMealLogRouter.put("/meal/:mealLogId", updateMealLog);
+userMealLogRouter.delete("/meal/:mealLogId", deleteMealLog);
 
 export default userMealLogRouter;
