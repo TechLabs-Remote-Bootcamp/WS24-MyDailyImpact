cec7cc48af7bb409b63e778cd10c50d25bbaa258
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-02-24 12:11:43 +0100

    clean code

--- mdi-react/src/components/pages/MealHistory/HistoryTable.jsx
+++ mdi-react/src/components/pages/MealHistory/HistoryTable.jsx
@@ -53,26 +53,8 @@ export default function HistoryTable() {
         setLoading(false);
       }
     };
-    // const convertToLogObject = () => {
-    //   if (!logs || logs.length === 0) {
-    //     console.log("Keine Logs zum Konvertieren vorhanden");
-    //     return [];
-    //   }
-    //   const convertedLogs = logs.map((log) => ({
-    //     mealName: log.mealName,
-    //     category: log.category,
-    //     date: formatDate(log.date),
-    //     notes: log.notes,
-    //     id: log._id, // Falls Sie die ID später brauchen
-    //     userId: log.userId, // Falls Sie die userId später brauchen
-    //   }));
-    //   console.log("Konvertierte Logs:", convertedLogs);
-    //   return convertedLogs;
-    // };
 
     fetchData();
-    // data = convertToLogObject();
-    // console.log(data[2]);
   }, []); // Only run once on mount
 
   // When data-set is still loading
@@ -118,21 +100,23 @@ export default function HistoryTable() {
                 <td>{item.mealName}</td>
                 <td>{item.notes}</td>
                 <td className={styles.actions}>
-                  <svg
-                    // width="22"
-                    // height="22"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 7H20M10 11V17M14 11V17M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7"
-                      stroke="#5055BA"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
+                  <button>
+                    <svg
+                      // width="22"
+                      // height="22"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 7H20M10 11V17M14 11V17M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7"
+                        stroke="#5055BA"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </button>
                 </td>
               </tr>
             ))}
