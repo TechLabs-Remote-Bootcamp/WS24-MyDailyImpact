7e4d6d515f03694d64f8107ec9c5314994085fa6
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-02-25 01:44:05 +0100

    v2 add total meals above of table

--- mdi-react/src/components/pages/MealHistory/HistoryTable.jsx
+++ mdi-react/src/components/pages/MealHistory/HistoryTable.jsx
@@ -103,45 +103,48 @@ export default function HistoryTable({ onDataSubmit }) {
   }
 
   return (
-    <section className={styles.formSection}>
-      <table className={styles.tableContainer}>
-        <thead className={styles.tableHead}>
-          <tr>
-            <th>Date</th>
-            <th>Category</th>
-            <th>Meal name</th>
-            <th>Notes</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody className={styles.tabelBody}>
-          {logs.map((item) => (
-            <tr key={item._id} name="logRow">
-              <td>{formatDate(item.date)}</td>
-              <td>{item.category}</td>
-              <td>{item.mealName}</td>
-              <td>{item.notes}</td>
-              <td className={styles.actions}>
-                <button onClick={() => deleteMealLog(item._id)}>
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 7H20M10 11V17M14 11V17M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7"
-                      stroke="#5055BA"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </button>
-              </td>
+    <>
+      <p className={styles.textLine}>Total logs: {count}</p>
+      <section className={styles.formSection}>
+        <table className={styles.tableContainer}>
+          <thead className={styles.tableHead}>
+            <tr>
+              <th>Date</th>
+              <th>Category</th>
+              <th>Meal name</th>
+              <th>Notes</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </section>
+          </thead>
+          <tbody className={styles.tabelBody}>
+            {logs.map((item) => (
+              <tr key={item._id} name="logRow">
+                <td>{formatDate(item.date)}</td>
+                <td>{item.category}</td>
+                <td>{item.mealName}</td>
+                <td>{item.notes}</td>
+                <td className={styles.actions}>
+                  <button onClick={() => deleteMealLog(item._id)}>
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 7H20M10 11V17M14 11V17M5 7L6 19C6 19.5304 6.21071 20.0391 6.58579 20.4142C6.96086 20.7893 7.46957 21 8 21H16C16.5304 21 17.0391 20.7893 17.4142 20.4142C17.7893 20.0391 18 19.5304 18 19L19 7M9 7V4C9 3.73478 9.10536 3.48043 9.29289 3.29289C9.48043 3.10536 9.73478 3 10 3H14C14.2652 3 14.5196 3.10536 14.7071 3.29289C14.8946 3.48043 15 3.73478 15 4V7"
+                        stroke="#5055BA"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </>
   );
 }
