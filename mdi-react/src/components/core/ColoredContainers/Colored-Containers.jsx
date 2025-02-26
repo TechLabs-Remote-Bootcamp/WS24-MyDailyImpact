b4c915380f4fffe4a85f62d722195daea28eb447
Mohnblumenberg <lillypoulos-home@yahoo.de>
2025-02-26 10:05:27 +0100

    add component description comment

--- mdi-react/src/components/core/ColoredContainers/Colored-Containers.jsx
+++ mdi-react/src/components/core/ColoredContainers/Colored-Containers.jsx
@@ -1,5 +1,7 @@
 import styles from "./Colored-Containers.module.scss";
 
+// 3 boxes for title (green box), subtitle (blue box) and content (textbox)
+
 export default function ColoredContainers({ h2Text, h3Text, children }) {
   return (
     <div className={styles.colorContainer}>
