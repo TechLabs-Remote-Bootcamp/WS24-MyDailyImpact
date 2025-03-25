188247c89baac53e7cbc6dd91bae5b697b6bc612
Vivian Sandler <161636280+VivianSandler@users.noreply.github.com>
2025-03-25 19:22:22 +0000

    Update RC_imprint.jsx

--- mdi-react/src/components/pages/Imprint/RC_imprint.jsx
+++ mdi-react/src/components/pages/Imprint/RC_imprint.jsx
@@ -8,26 +8,24 @@ export default function RC_imprint() {
         <div className={styles["imprint-text"]}>
           <h5>Owners: </h5>
           <p>
-            Techlabs Bootcamp team members, Vivian Sandler, Christine Stockert,
+            Techlabs Bootcamp team members: Vivian Sandler, Christine Stockert,
             Tomislav Varga, Vanessa Türker, Menglan Liu, A. von
             Bank, Elisabeth Prossinger, Kate Kozelkova
           </p>
 
-          <h5>Postal address: </h5>
-          <p>TechLabs e. V. Hafenweg 16 48155 Münster</p>
-
           <h5>Contact: </h5>
-          <p>mydailyimpact@gmail.com; info@techlabs.org</p>
+          <p>mydailyimpact@gmail.com
+            
           <h5>Responsible for this content: </h5>
-          <p>TechLabs & team members listed above</p>
+          <p>Team members listed above</p>
 
           <h5>Copyright: </h5>
           <p>
-            &copy;2025 MyDailyImpact &copy;2024 TechLabs; All rights reserved.
+            &copy;2025 MyDailyImpact All rights reserved.
             Text, images, graphics, sounds, animations and videos as well as
             their arrangement on the website are protected by copyright and
             other protective laws. The content of this website may not be
-            copied, distributed, modified, or made accessible to thrid parties
+            copied, distributed, modified, or made accessible to third parties
             for commercial purposes.
           </p>
         </div>
